f5f9d0ab5eb9dfbd6b97a7a4628985cc83b211a4
Niels Mentink <niels.mentink@outlook.com>
2016-09-14 22:21:06 +0200

    checking token at execution

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-var token = '297689787:AAGrd7zrK2oJWCOrieyUqpqelLjXoDvVS9w';
-var bot = new TelegramBot(token, {polling: true});
+var token = process.argv.slice(2);
 
-bot.onText(/\/echo (.+)/, function (msg, match) {
-  var fromId = msg.from.id;
-  var resp = match[1];
-  bot.sendMessage(fromId, resp);
-  console.log(resp);
-  console.log("test");
-});
+if (token.length > 0) {
+  var bot = new TelegramBot(token, {polling: true});
+  bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id;
+    var resp = match[1];
+    bot.sendMessage(fromId, resp);
+    console.log(resp);
+    console.log("test");
+  });
+} else {
+  console.log("Usage: \"node index.js BOTTOKENHERE\"");
+  process.exit(1);
+}
