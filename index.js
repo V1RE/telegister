56b461ae975bf4f2d40eb22c2780e76dd488d17d
Jasper Mulder <supenogaming@gmail.com>
2016-09-14 22:17:33 +0200

    added momentjs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
+const Magister = require('magister.js');
 
 var token = '297689787:AAGrd7zrK2oJWCOrieyUqpqelLjXoDvVS9w';
 var bot = new TelegramBot(token, {polling: true});
@@ -8,5 +9,15 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
   var resp = match[1];
   bot.sendMessage(fromId, resp);
   console.log(resp);
-  console.log("test");
+  
+  new Magister.Magister({
+    school: 'xxxx',
+    username: 'xxxx',
+	  password: 'xxxx'
+    }).ready(function () {
+	  this.appointments(new Date(), function (error, result) {
+		console.log(result[0].teachers()[0].fullName());
+	 });
+  });
 });
+
