5602f00cdf984ac54a8b89cced7862950fab5fc1
Niels Mentink <niels.mentink@outlook.com>
2016-09-14 22:47:38 +0200

    Split code up into files for better readability

---
+++ app.js
@@ -0,0 +1,24 @@
+const TelegramBot = require('node-telegram-bot-api');
+const Magister = require('magister.js');
+const Moment = require('moment');
+
+function mainbot (token) {
+  bot = new TelegramBot(token, {polling: true});
+  bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id;
+    var resp = match[1];
+    bot.sendMessage(fromId, resp);
+    console.log(resp);
+
+//    new Magister.Magister({
+//      school: 'xxxx',
+//      username: 'xxxx',
+//      password: 'xxxx'
+//    }).ready(function () {
+//      this.appointments(new Date(), function (error, result) {
+//      console.log(result[0].teachers()[0].fullName());
+//      });
+//    });
+    
+  });
+}
--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-const TelegramBot = require('node-telegram-bot-api');
-const Magister = require('magister.js');
-const Moment = require('moment');
+const fs = require('fs');
+
+eval(fs.readFileSync('app.js').toString());
 
 var token = process.argv.slice(2);
 
 if (token.length > 0) {
-  bot = new TelegramBot(token, {polling: true});
-  bot.onText(/\/echo (.+)/, function (msg, match) {
-    var fromId = msg.from.id;
-    var resp = match[1];
-    bot.sendMessage(fromId, resp);
-    console.log(resp);
-
-    new Magister.Magister({
-      school: 'xxxx',
-      username: 'xxxx',
-      password: 'xxxx'
-    }).ready(function () {
-      this.appointments(new Date(), function (error, result) {
-      console.log(result[0].teachers()[0].fullName());
-      });
-    });
-  });
+  mainbot(token)
 } else {
   console.log("Usage: \"node index.js BOTTOKENHERE\"");
   process.exit(1);
